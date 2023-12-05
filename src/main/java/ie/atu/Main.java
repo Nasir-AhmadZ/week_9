ddac4ae0eab1b7bff7f4c15cabe57db5dcf9e328
Nasir Ahmad <G00419630@atu.ie>
2023-12-05 14:14:39 +0000

    completed part 1 and 2

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -4,10 +4,15 @@ package ie.atu;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Person myPerson = new Person("Paul","Kinvarra","3333");
-        System.out.println(myPerson.toString());
+        Customer Part1 = new Customer("Paul","Kinvarra","3333","3333f",true);
+        System.out.println(Part1.toString());
 
-        Customer myCustomer = new Customer("Paul","Kinvarra","3333","3333f",true);
-        System.out.println(myCustomer.toString());
+        Customer Part2 = new Customer();
+        Part2.setName("Paul");
+        Part2.setAddress("Kinvarra");
+        Part2.setPhone("3333");
+        Part2.setCustomerNumber("3333f");
+        Part2.setMailingList(true);
+        System.out.println(Part2.toString());
     }
 }
